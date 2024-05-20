d3d981257b50c52067d98df58d2d2bf3acfa2646
ta241003 <ngotheanh241003@gmail.com>
2024-05-20 21:25:46 +0700

    Save user login session information

--- App.js
+++ App.js
@@ -1,29 +1,100 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import SplashScreen from "./screens/SplashScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { enableScreens } from "react-native-screens";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import COLORS from "./constants/colors";
+import { FontAwesome } from "@expo/vector-icons";
+import Welcome from "./screens/Welcome";
 import Login from "./screens/Login";
+import MainApp from "./screens/MainApp";
+import Signup from "./screens/Signup";
+import ConfirmEmail from "./screens/ConfirmEmail";
+import ConfirmCode from "./screens/ConfirmCode";
+import ConfirmPassword from "./screens/ConfirmPassword";
+import FAQ from "./screens/FAQ";
+import MyApplied from "./screens/MyApplied";
+import { firebase } from "./configFirebase";
 import HomePage from "./screens/HomePage";
 
-const Stack = createStackNavigator();
+enableScreens();
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+const WelcomeStack = () => {
+	const [initializing, setInitializing] = useState(true);
+	const [user, setUser] = useState();
+	const [isFirstLaunch, setIsFirstLaunch] = useState(null);
+
+	const checkFirstLaunch = async () => {
+		try {
+			const value = await AsyncStorage.getItem("isFirstLaunch");
+			if (value === null) {
+				// No value set, meaning this is the first launch
+				await AsyncStorage.setItem("isFirstLaunch", "false");
+				setIsFirstLaunch(true);
+			} else {
+				setIsFirstLaunch(false);
+			}
+		} catch (error) {
+			console.error("Error checking first launch", error);
+		}
+	};
+
+	useEffect(() => {
+		checkFirstLaunch();
+		const subscriber = firebase
+			.auth()
+			.onAuthStateChanged(onAuthStateChanged);
+		return subscriber; // unsubscribe on unmount
+	}, []);
+
+	function onAuthStateChanged(user) {
+		setUser(user);
+		if (initializing) setInitializing(false);
+	}
+
+	if (initializing || isFirstLaunch === null) return null;
+
+	return (
+		<Stack.Navigator
+			initialRouteName={
+				!user ? (isFirstLaunch ? "Welcome" : "Login") : "MainApp"
+			}
+			screenOptions={{ headerShown: false }}
+		>
+			{!user ? (
+				<>
+					<Stack.Screen name="Welcome" component={Welcome} />
+					<Stack.Screen name="Login" component={Login} />
+					<Stack.Screen name="Signup" component={Signup} />
+					<Stack.Screen
+						name="ConfirmEmail"
+						component={ConfirmEmail}
+					/>
+					<Stack.Screen name="ConfirmCode" component={ConfirmCode} />
+					<Stack.Screen
+						name="ConfirmPassword"
+						component={ConfirmPassword}
+					/>
+					<Stack.Screen name="FAQ" component={FAQ} />
+				</>
+			) : (
+				<Stack.Screen name="MainApp" component={MainApp} />
+			)}
+		</Stack.Navigator>
+	);
+};
 
 const App = () => {
 	return (
-		<NavigationContainer>
-			<Stack.Navigator initialRouteName="SplashScreen">
+		<NavigationContainer independent={true}>
+			<Stack.Navigator>
 				<Stack.Screen
-					name="SplashScreen"
-					component={SplashScreen}
-					options={{ headerShown: false }}
-				/>
-				<Stack.Screen
-					name="Login"
-					component={Login}
-					options={{ headerShown: false }}
-				/>
-				<Stack.Screen
-					name="HomePage"
-					component={HomePage}
+					name="WelcomeStack"
+					component={WelcomeStack}
 					options={{ headerShown: false }}
 				/>
 			</Stack.Navigator>
--- screens/welcome.style.js
+++ screens/welcome.style.js
@@ -1,34 +1,200 @@
 import { StyleSheet } from "react-native";
 
-export default StyleSheet.create({
+import { COLORS, FONT, SIZES } from "../constants";
+
+const styles = StyleSheet.create({
+	container: {
+		width: "100%",
+		marginHorizontal: 20,
+	},
 	head: {
-		flexDirection: "row",
-		justifyContent: "space-between",
-		padding: 16,
+		flexDirection: "row", // Sắp xếp các phần tử trong hàng ngang
+		justifyContent: "space-between", // Canh giữa hai hình ảnh
+		alignItems: "center", // Canh chỉnh theo trục dọc
+		paddingHorizontal: 20, // Thêm padding nếu cần thiết
 	},
 	menu: {
-		width: 24,
-		height: 24,
+		marginTop: 50,
+		marginBottom: 20,
+		width: 30,
+		height: 30,
+		// borderRadius: 10,
 	},
 	avatar: {
-		width: 32,
-		height: 32,
-		borderRadius: 16,
-	},
-	container: {
-		padding: 16,
+		marginTop: 60,
+		marginBottom: 20,
+		width: 60,
+		height: 60,
+		borderRadius: 20, // Để bo tròn hình ảnh
 	},
 	userName: {
-		fontSize: 24,
+		// fontFamily: FONT.regular,
+		fontSize: SIZES.large,
+		color: COLORS.secondary,
 		fontWeight: "bold",
 	},
 	welcomeMessage: {
-		fontSize: 18,
-		marginTop: 8,
+		// fontFamily: FONT.bold,
+		fontSize: SIZES.xLarge,
+		color: COLORS.primary,
+		marginTop: 2,
+		fontWeight: "bold",
+		fontSize: 28,
 	},
-	resetPassword: {
+	searchContainer: {
+		justifyContent: "center",
+		alignItems: "center",
+		flexDirection: "row",
+		marginTop: SIZES.large,
+		height: 50,
+		marginHorizontal: 20,
+	},
+	searchWrapper: {
+		flex: 1,
+		backgroundColor: COLORS.white,
+		marginRight: SIZES.small,
+		justifyContent: "center",
+		alignItems: "center",
+		borderRadius: SIZES.medium,
+		height: "100%",
+		backgroundColor: "#E6E4E6",
+	},
+	searchInput: {
+		// fontFamily: FONT.regular,
+		width: "100%",
+		height: "100%",
+		paddingHorizontal: SIZES.medium,
+		fontSize: 17,
+	},
+	searchBtn: {
+		width: 50,
+		height: "100%",
+		backgroundColor: COLORS.tertiary,
+		borderRadius: SIZES.medium,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	searchBtnImage: {
+		width: "50%",
+		height: "50%",
+		tintColor: COLORS.lightWhite,
+	},
+	tabsContainer: {
+		width: "100%",
+		marginTop: SIZES.medium,
+		paddingHorizontal: 20,
+	},
+	row: {
+		marginHorizontal: 20,
+	},
+	subrow: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+		marginTop: 20,
+		marginBottom: 15,
+	},
+	boldText: {
+		fontWeight: "bold",
+		fontSize: 20,
+	},
+	showAll: {
+		color: "#83829A",
 		fontSize: 16,
-		color: "blue",
-		marginTop: 8,
+	},
+	companyList: {
+		// paddingHorizontal: 10,
+	},
+	companyCard: {
+		backgroundColor: "#312651",
+		borderRadius: 20,
+		padding: 10,
+		marginRight: 10,
+		alignItems: "flex-start",
+		flexDirection: "row",
+		justifyContent: "space-between",
+		position: "relative",
+		width: 200,
+	},
+	logo_container: {
+		width: 60,
+		height: 60,
+		backgroundColor: "#fff",
+		borderRadius: 10,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	companyLogo: {
+		width: 50,
+		height: 50,
+	},
+	companyInfo: {
+		marginTop: 10,
+	},
+	companyName: {
+		fontSize: 14,
+		color: COLORS.white,
+		marginBottom: 3,
+	},
+	jobName: {
+		fontSize: 16,
+		fontWeight: "bold",
+		marginBottom: 3,
+		color: COLORS.white,
+	},
+	jobDescription: {
+		fontSize: 14,
+		color: COLORS.white,
+	},
+	heartIcon: {
+		position: "absolute",
+		top: 15,
+		right: 20,
+		color: "#ff7754",
+	},
+	tab: (activeJobType, item) => ({
+		paddingVertical: SIZES.small / 2,
+		paddingHorizontal: SIZES.small,
+		borderRadius: SIZES.medium,
+		borderWidth: 1,
+		borderColor: activeJobType === item ? COLORS.secondary : COLORS.gray2,
+	}),
+	tabText: (activeJobType, item) => ({
+		// fontFamily: FONT.medium,
+		color: activeJobType === item ? COLORS.secondary : COLORS.gray2,
+	}),
+
+	nearby_job_container: {
+		borderRadius: 10,
+		flexDirection: "row",
+		marginBottom: 10,
+		backgroundColor: "#fafafa",
+		padding: 10,
+	},
+	logo: {
+		width: 70,
+		height: 70,
+		marginRight: 20,
+		borderRadius: 50,
+	},
+	textContainer: {
+		flex: 1,
+	},
+	company: {
+		fontSize: 17,
+		marginBottom: 3,
+		fontWeight: "bold",
+	},
+	jobname: {
+		fontSize: 17,
+		color: "#555555",
+		fontWeight: "bold",
+		marginBottom: 2,
+	},
+	describe: {
+		fontSize: 15,
+		color: "#555555",
 	},
 });
+
+export default styles;
